8b2eb3e8cbf8fe98a55aab496bf4f8652dc7964e
Callum Turnbull <callum.turnbull@digital.hmrc.gov.uk>
2016-03-15 16:13:13 +0000

    NISP-1732: Removed exclusion Breadcrumb

--- app/uk/gov/hmrc/nisp/utils/Breadcrumb.scala
+++ app/uk/gov/hmrc/nisp/utils/Breadcrumb.scala
@@ -40,8 +40,7 @@ trait Breadcrumb {
       "account" -> ((URLEncoder.encode(Messages("nisp.breadcrumb.pension"), "UTF-8"), routes.AccountController.show().url)),
       "nirecord" -> ((URLEncoder.encode(Messages("nisp.breadcrumb.nirecord"), "UTF-8"), routes.NIRecordController.showGaps().url)),
       "voluntarycontribs" -> ((URLEncoder.encode(Messages("nisp.breadcrumb.nirecord.voluntaryContrib"), "UTF-8"), routes.NIRecordController.showVoluntaryContributions().url)),
-      "gapsandhowtocheck" -> ((URLEncoder.encode(Messages("nisp.breadcrumb.nirecord.gapsandhowtocheck"), "UTF-8"), routes.NIRecordController.showGapsAndHowToCheckThem().url)),
-      "exclusion" -> ((URLEncoder.encode(Messages("nisp.breadcrumb.account"), "UTF-8"), applicationConfig.pertaxFrontendUrl))
+      "gapsandhowtocheck" -> ((URLEncoder.encode(Messages("nisp.breadcrumb.nirecord.gapsandhowtocheck"), "UTF-8"), routes.NIRecordController.showGapsAndHowToCheckThem().url))
     )
 
     val items: List[Option[(String, String)]] = request.path.split("/").filter(!_.isEmpty).map(links.get).toList
