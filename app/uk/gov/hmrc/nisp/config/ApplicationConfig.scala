4fe284f950b7b23526f5e5ce78439c58076fd47b
gus.principe <gus.principe@digital.hmrc.gov.uk>
2016-07-26 10:56:43 +0100

    NISP-1917B Switch is working now

--- app/uk/gov/hmrc/nisp/config/ApplicationConfig.scala
+++ app/uk/gov/hmrc/nisp/config/ApplicationConfig.scala
@@ -82,5 +82,5 @@ object ApplicationConfig extends ApplicationConfig with ServicesConfig {
   private val pertaxFrontendService: String = baseUrl("pertax-frontend")
   override lazy val pertaxFrontendUrl: String = configuration.getString(s"breadcrumb-service.url").getOrElse("")
   override lazy val breadcrumbPartialUrl: String = s"$pertaxFrontendService/personal-account/integration/main-content-header"
-  override val showGovUkFullYearPage: Boolean = configuration.getBoolean("govuk-full-year-page.enabled").getOrElse(true)
+  override val showGovUkFullYearPage: Boolean = configuration.getBoolean("microservice.services.features.fullNIrecord").getOrElse(false)
 }
