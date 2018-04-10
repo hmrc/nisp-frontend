63f391808b4181d774008f33b1631b9fb2f2cca8
neha.kannaujia <neha.kannaujia@gmail.com>
2018-04-10 11:13:15 +0100

    DDCNLS-2999: Added redirect page

---
+++ app/uk/gov/hmrc/nisp/controllers/RedirectController.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.nisp.controllers
+
+import play.api.Logger
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.nisp.config.ApplicationConfig
+import uk.gov.hmrc.nisp.connectors.{IdentityVerificationConnector, IdentityVerificationSuccessResponse}
+import uk.gov.hmrc.nisp.controllers.auth.AuthorisedForNisp
+import uk.gov.hmrc.nisp.controllers.connectors.AuthenticationConnectors
+import uk.gov.hmrc.nisp.controllers.partial.PartialRetriever
+import uk.gov.hmrc.nisp.services.CitizenDetailsService
+import uk.gov.hmrc.play.frontend.auth.Actions
+import uk.gov.hmrc.play.frontend.controller.UnauthorisedAction
+import scala.concurrent.Future
+
+object RedirectController extends RedirectController with AuthenticationConnectors with PartialRetriever {
+  override val citizenDetailsService: CitizenDetailsService = CitizenDetailsService
+  override val applicationConfig: ApplicationConfig = ApplicationConfig
+  override val identityVerificationConnector: IdentityVerificationConnector = IdentityVerificationConnector
+}
+
+trait RedirectController extends NispFrontendController with Actions with AuthorisedForNisp {
+  val identityVerificationConnector: IdentityVerificationConnector
+
+  def show: Action[AnyContent] = UnauthorisedAction(
+    implicit request =>
+    Ok(uk.gov.hmrc.nisp.views.html.redirect()).withNewSession
+  )
+}
