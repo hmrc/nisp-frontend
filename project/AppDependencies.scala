6062142c23c26c25ec81d8c8d105f83d38a4b3f6
Steven Truong <45595406+oc-ste@users.noreply.github.com>
2019-06-25 10:40:25 +0100

    refactored AppDependencies to remove unused TestDependencies and inlined version numbers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,50 +2,26 @@ import sbt._
 
 object AppDependencies {
 
-  val frontendBootstrapVersion = "12.9.0"
-  val playLanguageVersion = "3.4.0"
-  val httpCachingClientVersion = "8.1.0"
-  val cspClientVersion = "3.1.0"
-  val localTemplateRendererVersion = "2.3.0"
-  val taxYearVersion="0.5.0"
-  val playConditionalFormMappingVersion="0.2.0"
-  val playBreadCrumbVersion="1.0.0"
-
   val compile = Seq(
-    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
-    "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-    "uk.gov.hmrc" %% "play-language" % playLanguageVersion,
-    "uk.gov.hmrc" %% "tax-year" % taxYearVersion,
-    "uk.gov.hmrc" %% "csp-client" % cspClientVersion,
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % playConditionalFormMappingVersion,
-    "uk.gov.hmrc" %% "local-template-renderer"  % localTemplateRendererVersion,
-    "uk.gov.hmrc" %% "play-breadcrumb"  %  playBreadCrumbVersion
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "12.9.0",
+    "uk.gov.hmrc" %% "http-caching-client" % "8.1.0",
+    "uk.gov.hmrc" %% "play-language" % "3.4.0",
+    "uk.gov.hmrc" %% "tax-year" % "0.5.0",
+    "uk.gov.hmrc" %% "csp-client" % "3.1.0",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "0.2.0",
+    "uk.gov.hmrc" %% "local-template-renderer"  % "2.3.0",
+    "uk.gov.hmrc" %% "play-breadcrumb"  % "1.0.0"
   )
 
-  trait TestDependencies {
-    lazy val scope: String = "test"
-    lazy val test: Seq[ModuleID] = ???
-  }
+  val test = Seq(
+      "org.pegdown" % "pegdown" % "1.6.0",
+      "org.jsoup" % "jsoup" % "1.10.2",
+      "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1",
+      "uk.gov.hmrc" %% "hmrctest" % "2.3.0",
+      "org.mockito" % "mockito-core" % "2.6.3"
+  ).map(_ % "test")
 
-  object Test {
-    val hmrcTestVersion = "2.3.0"
-    val pegdownVersion = "1.6.0"
-    val jsoupVersion = "1.10.2"
-    val scalaTestPlusVersion = "1.5.1"
-    val mockitoCoreVersion = "2.6.3"
-
-    def apply() = new TestDependencies {
-      override lazy val test = Seq(
-        "org.pegdown" % "pegdown" % pegdownVersion % scope,
-        "org.jsoup" % "jsoup" % jsoupVersion % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusVersion % scope,
-        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        "org.mockito" % "mockito-core" % mockitoCoreVersion
-      )
-    }.test
-  }
   
-  val all: Seq[ModuleID] = compile ++ Test()
-
+  val all: Seq[ModuleID] = compile ++ test
 }
 
