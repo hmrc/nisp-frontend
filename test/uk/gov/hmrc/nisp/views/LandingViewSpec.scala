288bf4bc236ddc45bba37f67ad84ba602ec185b2
Dandyboyo2 <daniel.alvey@digital.hmrc.gov.uk>
2020-03-18 16:14:44 +0000

    DDCNL-17 | refactored last review comment

--- test/uk/gov/hmrc/nisp/views/LandingViewSpec.scala
+++ test/uk/gov/hmrc/nisp/views/LandingViewSpec.scala
@@ -27,7 +27,7 @@ import uk.gov.hmrc.nisp.controllers.FeedbackController
 import uk.gov.hmrc.nisp.controllers.auth.NispAuthedUser
 import uk.gov.hmrc.nisp.fixtures.NispAuthedUserFixture
 import uk.gov.hmrc.nisp.helpers._
-import uk.gov.hmrc.nisp.utils.{Constants, MockTemplateRenderer}
+import uk.gov.hmrc.nisp.utils.{Constants, FakeApplicationConfig, MockTemplateRenderer}
 import uk.gov.hmrc.nisp.views.html.landing
 import uk.gov.hmrc.play.partials.{CachedStaticHtmlPartialRetriever, FormPartialRetriever}
 import uk.gov.hmrc.renderer.TemplateRenderer
@@ -50,34 +50,7 @@ class LandingViewSpec extends HtmlSpec with MockitoSugar {
 
     override def contactFormReferer(implicit request: Request[AnyContent]): String = request.headers.get(REFERER).getOrElse("")
 
-    override val applicationConfig: ApplicationConfig = new ApplicationConfig {
-      override val ggSignInUrl: String = ""
-      override val verifySignIn: String = ""
-      override val verifySignInContinue: Boolean = false
-      override val assetsPrefix: String = ""
-      override val reportAProblemNonJSUrl: String = ""
-      override val ssoUrl: Option[String] = None
-      override val identityVerification: Boolean = false
-      override val betaFeedbackUnauthenticatedUrl: String = ""
-      override val notAuthorisedRedirectUrl: String = ""
-      override val contactFrontendPartialBaseUrl: String = ""
-      override val govUkFinishedPageUrl: String = ""
-      override val showGovUkDonePage: Boolean = false
-      override val analyticsHost: String = ""
-      override val betaFeedbackUrl: String = ""
-      override val analyticsToken: Option[String] = None
-      override val reportAProblemPartialUrl: String = ""
-      override val contactFormServiceIdentifier: String = "NISP"
-      override val postSignInRedirectUrl: String = ""
-      override val ivUpliftUrl: String = ""
-      override val pertaxFrontendUrl: String = ""
-      override val breadcrumbPartialUrl: String = ""
-      override lazy val showFullNI: Boolean = false
-      override val futureProofPersonalMax: Boolean = false
-      override val isWelshEnabled = false
-      override val frontendTemplatePath: String = "microservice.services.frontend-template-provider.path"
-      override val feedbackFrontendUrl: String = "/foo"
-    }
+    override val applicationConfig: ApplicationConfig = new FakeApplicationConfig{}
   }
 
   implicit val cachedStaticHtmlPartialRetriever = MockCachedStaticHtmlPartialRetriever
