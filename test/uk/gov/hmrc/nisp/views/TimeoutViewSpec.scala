7bb51b7bb0ea2589f89ad9418aa388daef4f3cb4
John Lewis <23009934+lewey@users.noreply.github.com>
2021-02-18 14:23:33 +0000

    missing-tests: fixed broken tests for TimeoutViewSpec

--- test/uk/gov/hmrc/nisp/views/TimeoutViewSpec.scala
+++ test/uk/gov/hmrc/nisp/views/TimeoutViewSpec.scala
@@ -43,37 +43,37 @@ class TimeoutViewSpec extends HtmlSpec with MockitoSugar {
     }
 
     "assert correct heading title on page" in {
-      val title = source.getElementsByTag("h1").get(0).toString
+      val heading = source.getElementsByTag("h1").get(0).toString
       val messageKey = "nisp.iv.failure.timeout.title"
       val expected = messages(messageKey)
-      title must include(expected)
+      heading must include(expected)
     }
 
     "assert correct paragraph one text on page" in {
-      val title = source.getElementsByTag("p").get(1).toString
+      val paragraph = source.getElementsByTag("p").get(1).toString
       val messageKey = "nisp.iv.failure.timeout.message"
       val expected = messages(messageKey)
-      title must include(expected)
+      paragraph must include(expected)
     }
 
     "assert correct paragraph two text on page" in {
-      val title = source.getElementsByTag("p").get(2).toString
+      val paragraph = source.getElementsByTag("p").get(2).toString
       val messageKey = "nisp.iv.failure.timeout.data"
       val expected = messages(messageKey)
-      title must include(expected)
+      paragraph must include(expected)
     }
 
     "assert correct button text on page" in {
-      val title = source.getElementsByTag("a").get(1).toString
+      val button = source.getElementsByClass("button").text
       val messageKey = "nisp.iv.failure.timeout.button"
       val expected = messages(messageKey)
-      title must include(expected)
+      button must include(expected)
     }
 
     "assert correct href on the start again button" in {
-      val redirect = source.getElementsByTag("a").get(1).attr("href")
+      val buttonHref = source.getElementsByClass("button").attr("href")
       val expected = "/check-your-state-pension/account"
-      redirect must include(expected)
+      buttonHref must include(expected)
     }
   }
 }
