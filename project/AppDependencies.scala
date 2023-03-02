a0c3947bc1db5a1bb1c8aab4d74a7690ad20dc49
Derek Gray <derek.gray@digital.hmrc.gov.uk>
2023-03-02 14:32:16 +0000

    Upgrade libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,13 +10,13 @@ object AppDependencies {
     "uk.gov.hmrc"   %% "http-caching-client"           % "10.0.0-play-28",
     "uk.gov.hmrc"   %% "tax-year"                      % "3.0.0",
     "uk.gov.hmrc"   %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "3.32.0-play-28",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "6.7.0-play-28",
     "uk.gov.hmrc"   %% "play-frontend-pta"             % "0.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.pegdown"             %   "pegdown"                 % "1.6.0",
-    "org.jsoup"               %   "jsoup"                   % "1.14.3",
+    "org.jsoup"               %   "jsoup"                   % "1.15.4",
     "uk.gov.hmrc"             %%  "bootstrap-test-play-28"  % bootstrapVersion,
     "org.mockito"             %   "mockito-core"            % "4.6.1",
     "com.github.tomakehurst"  %   "wiremock-jre8"           % "2.27.2",
