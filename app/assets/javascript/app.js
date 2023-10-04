b6d3955ee58f35d695bdb4a63488aea5fcc66d1c
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2023-10-04 13:25:48 +0100

    DDCNL-7950: Removed more unused JS

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,66 +1,65 @@
 
 function hideDetails() {
-  const details =
-      document
-          .querySelector('.hide-details')
+  const details = document.querySelector('.hide-details')
 
-  if (details != null) {
-    details.style.display = "none"
+  if (details != null && details != 'undefined') {
+    details.style.display = 'none'
   }
 }
 
 function hideAccordion() {
-  const dd =
-      document
-          .querySelector('.accordion')
-          .getElementsByTagName('dd')
+  const accordion = document.querySelector('.accordion')
 
-  if (dd != null && dd.length > 0) {
-    for (let i = 0; i < dd.length; i++) {
-      dd[i].style.display = "none"
+  if (accordion != null && accordion != 'undefined') {
+    const dd = accordion.getElementsByTagName('dd')
+
+    if (dd != null && dd.length > 0) {
+      for (let i = 0; i < dd.length; i++) {
+        dd[i].style.display = 'none'
+      }
     }
   }
 }
 
-function accordionShowHide() {
-  const expandables =
-      document
-          .getElementsByClassName('expandable')
+function accordionToggleShowHide() {
+  const expandables = document.getElementsByClassName('expandable')
 
   if (expandables != null && expandables.length > 0) {
-    console.log(expandables.length)
     for (let i = 0; i < expandables.length; i++) {
-      let expandable = expandables[i]
-      expandable.onclick = function (e) {
+      expandables[i].addEventListener('click', function (e) {
         e.preventDefault();
         if (this.getAttribute('class') !== 'active') {
           this.setAttribute('class', 'active')
-          this.nextElementSibling.style.display = "block"
+          this.nextElementSibling.style.display = 'block'
           this.querySelector('.view-details').setAttribute('aria-expanded', 'true')
           this.setAttribute('aria-expanded', 'true')
         } else {
           this.removeAttribute('class')
-          this.nextElementSibling.style.display = "none"
+          this.nextElementSibling.style.display = 'none'
           this.querySelector('.view-details').setAttribute('aria-expanded', 'false')
           this.setAttribute('aria-expanded', 'false')
         }
-      }
+      });
     }
+  }
+}
+
+function printLink() {
+  const printLink = document.getElementById('printLink');
+
+  if (printLink != null && printLink != 'undefined') {
+    printLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      window.print();
+    });
   }
 }
 
 document.addEventListener('DOMContentLoaded', function (){
   hideDetails();
   hideAccordion();
-  accordionShowHide();
+  accordionToggleShowHide();
+  printLink();
 }, false);
 
-const printlink = document.getElementById('printLink');
 
-if(printlink != null && printlink != 'undefined' ) {
-
-  printlink.addEventListener("click", function (e) {
-    e.preventDefault();
-    window.print();
-  });
-}
