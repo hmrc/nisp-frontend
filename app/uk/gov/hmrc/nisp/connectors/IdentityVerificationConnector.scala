566fe696ca76cbd448fbca3c73343c72028c46f9
Dan Alvey <45761631+Dandyboyo2@users.noreply.github.com>
2024-02-12 12:24:48 +0000

    hmrcCopyrightHeaders | manual update headers for 2024 (#524)

--- app/uk/gov/hmrc/nisp/connectors/IdentityVerificationConnector.scala
+++ app/uk/gov/hmrc/nisp/connectors/IdentityVerificationConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ class IdentityVerificationConnector @Inject() (
         context.stop()
         result
       }
-      .map { 
+      .map {
         case Right(response) =>
           val result = (response.json \ "result").as[String]
           IdentityVerificationSuccessResponse(result)
