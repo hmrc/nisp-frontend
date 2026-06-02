0fd83ba500448a1b5373e5257a92321a499c3d8a
Balasubrahmanyam <173818211+bsgangapatnam@users.noreply.github.com>
2026-06-02 15:19:27 +0100

    refactored the mqp messages

--- test/uk/gov/hmrc/nisp/views/statePension/StatePensionViewSpec.scala
+++ test/uk/gov/hmrc/nisp/views/statePension/StatePensionViewSpec.scala
@@ -839,12 +839,12 @@ class StatePensionViewSpec
           }
 
           // MQP message
-          "render page with text 'You have no qualifying years on your National Insurance record. You usually need at least 10 qualifying years on your National Insurance record to get any State Pension.'" in {
+          "render page with text 'You have no qualifying years on your National Insurance record.'" in {
             mockSetup
             assertEqualsText(
               doc,
               "[data-spec='state_pension__no_qualifying_years']",
-              messages("nisp.mqp.currentQualifyingYears.zero", 10)
+              messages("nisp.mqp.currentQualifyingYears.zero")
             )
           }
 
